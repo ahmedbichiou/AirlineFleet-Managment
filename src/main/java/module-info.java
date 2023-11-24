45a9609bf98ae07a41083b231e0c78df28a76049
ahmedbichiou <95566685+ahmedbichiou@users.noreply.github.com>
2023-11-24 22:24:10 +0100

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany.login {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.login to javafx.fxml;
-    opens Controllers to javafx.fxml;
-    exports com.mycompany.login;
-}
